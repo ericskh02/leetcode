8281f3f05c9651fc599b4d70483002b12a02d06b
ericskh02 <42265526+ericskh02@users.noreply.github.com>
2021-03-25 18:47:33 +0800

    Create 1046. Last Stone Weight.cpp

---
+++ Heap/1046. Last Stone Weight.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> q(stones.begin(),stones.end());
+        while(q.size()>1){
+            int stone1 = q.top();
+            q.pop();
+            int stone2 = q.top();
+            q.pop();
+            if(stone1!=stone2){
+                q.push(stone1-stone2);
+            }
+        }
+        return (q.empty()) ? 0 : q.top();
+    }
+};
