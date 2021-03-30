95bcc8ec0346f2951644f2fb821b8a03960d105b
ericskh02 <42265526+ericskh02@users.noreply.github.com>
2021-03-30 15:26:49 +0800

    Update 39. Combination Sum.cpp

--- Backtracking/39. Combination Sum.cpp
+++ Backtracking/39. Combination Sum.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void dfs(vector<int>& candidates, int target, int i, int sum, vector<int>& combine)     {
-        if(target==sum){
-            ans.push_back(combine);
+    void dfs(vector<int>& candidates, int target, int sum, int i,vector<int>& combine){
+        if(sum==target){
+            ans.emplace_back(combine);
             return;
-        }
+        }    
         if(i>=candidates.size()){
             return;
         }
-        dfs(candidates,target,i+1,sum,combine);
-        if(sum+candidates[i]<=target){
-            combine.push_back(candidates[i]);
-            dfs(candidates,target,i,sum+candidates[i],combine);
+        for(int j = i;j<candidates.size() && candidates[j]+sum<=target;j++){
+            combine.push_back(candidates[j]);
+            dfs(candidates,target,sum+candidates[j],j,combine);
             combine.pop_back();
         }
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());   
         vector<int> combine;
-        sort(candidates.begin(),candidates.end());
         dfs(candidates,target,0,0,combine);
         return ans;
     }
